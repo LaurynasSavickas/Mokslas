ce1eb804fd407bab858271b393bd206f7d7745df
LaurynasSavickas <savas.laurynas@gmail.com>
2022-11-09 00:03:18 +0200

    Orders

---
+++ orders.js
@@ -0,0 +1,7 @@
+let wraps = 0;
+const addProduct = 1;
+
+const addWrap = (allWraps, addProduct) => (allWraps + addProduct);
+
+
+
---
+++ play.js
@@ -0,0 +1,20 @@
+let uname = 'Max';
+let uage = 29;
+let uhasHobbies = true;
+
+const summarizeUser = (userName, userAge, userHobbies) =>{
+    return(
+        'N: ' +
+        userName +
+        ' A: ' + 
+        userAge +
+        ' H: ' +
+        userHobbies
+    );
+};
+ const add= (a,b) => a + b;
+ 
+
+ console.log(add(1, 2));
+
+console.log(summarizeUser(uname, uage, uhasHobbies ));
